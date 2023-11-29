56d320fd6bfbb65abbe440912477d85cdc8713e9
Kyle Grealis <kyleGrealis@icloud.com>
2023-11-29 13:09:29 -0600

    initial commit; connect with API

--- R/app.R
+++ R/app.R
@@ -12,32 +12,43 @@ library(jsonlite)
 library(httr)
 
 ui <- fluidPage(
-  numericInput("input1", "Input 1", value = 0),
-  numericInput("input2", "Input 2", value = 0),
-  numericInput("input3", "Input 3", value = 0),
-  numericInput("input4", "Input 4", value = 0),
+  numericInput("input1", "Bill length (mm)", value = 46.8),
+  numericInput("input2", "Bill depth (mm)", value = 16.1),
+  numericInput("input3", "Flipper length (mm)", value = 0),
+  numericInput("input4", "Body mass (g)", value = 5500),
   actionButton("submit", "Submit"),
-  textOutput("prediction")
+  verbatimTextOutput("prediction")
 )
 
 server <- function(input, output) {
   prediction <- eventReactive(input$submit, {
     # Construct the request
-    url <- "https://positconnect.med.miami.edu/content/6ceebf29-1af5-4606-88d0-0b82f9e0223b/openapi.json"
+    url <- "https://positconnect.med.miami.edu/content/6ceebf29-1af5-4606-88d0-0b82f9e0223b/predict"
     body <- list(
-      input1 = input$input1,
-      input2 = input$input2,
-      input3 = input$input3,
-      input4 = input$input4
+      bill_length_mm    = input$input1,
+      bill_depth_mm     = input$input2,
+      flipper_length_mm = input$input3,
+      body_mass_g       = input$input4
     )
     
+    # Convert the body to JSON
+    body_json <- toJSON(body)
+    
     # Send the request and parse the response
-    response <- POST(url, body = body)
-    content(response)$probability
+    response <- POST(
+      url, body = body_json, 
+      add_headers("Content-Type" = "application/json"),
+      timeout(60)
+    )
+    json_response <- content(response, "parsed")
+    
+    # Convert the JSON response to a string
+    json_string <- toJSON(json_response, pretty = TRUE)
+    json_string
   })
   
   output$prediction <- renderText({
-    paste("Predicted probability:", prediction())
+    prediction()
   })
 }
 
