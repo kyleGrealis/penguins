74479e25b9122a05365693d232105575d1e0fea6
Kyle Grealis <kyleGrealis@icloud.com>
2023-11-29 13:42:29 -0600

    feat: added API token authorization to access model

--- R/app.R
+++ R/app.R
@@ -12,10 +12,10 @@ library(jsonlite)
 library(httr)
 
 ui <- fluidPage(
-  numericInput("input1", "Bill length (mm)", value = 46.8),
-  numericInput("input2", "Bill depth (mm)", value = 16.1),
-  numericInput("input3", "Flipper length (mm)", value = 0),
-  numericInput("input4", "Body mass (g)", value = 5500),
+  numericInput("bill_length_mm", "Bill length (mm)", value = 46.8),
+  numericInput("bill_depth_mm", "Bill depth (mm)", value = 16.1),
+  numericInput("flipper_length_mm", "Flipper length (mm)", value = 0),
+  numericInput("body_mass_g", "Body mass (g)", value = 5500),
   actionButton("submit", "Submit"),
   verbatimTextOutput("prediction")
 )
@@ -24,27 +24,30 @@ server <- function(input, output) {
   prediction <- eventReactive(input$submit, {
     # Construct the request
     url <- "https://positconnect.med.miami.edu/content/6ceebf29-1af5-4606-88d0-0b82f9e0223b/predict"
-    body <- list(
-      bill_length_mm    = input$input1,
-      bill_depth_mm     = input$input2,
-      flipper_length_mm = input$input3,
-      body_mass_g       = input$input4
+    query <- list(
+      bill_length_mm = input$bill_length_mm,
+      bill_depth_mm = input$bill_depth_mm,
+      flipper_length_mm = input$flipper_length_mm,
+      body_mass_g = input$body_mass_g
+    )
+    response <- GET(
+      url, 
+      query = query, 
+      # this key is from the Posit Connect API and stored in the .Renviron file
+      add_headers(Authorization = paste("Bearer", token = Sys.getenv('penguin_key')))
     )
     
-    # Convert the body to JSON
-    body_json <- toJSON(body)
+    # print the response to the console to debug
+    # print(response)
     
-    # Send the request and parse the response
-    response <- POST(
-      url, body = body_json, 
-      add_headers("Content-Type" = "application/json"),
-      timeout(60)
-    )
-    json_response <- content(response, "parsed")
+    # If the API is returning JSON:
+    result <- content(response, "parsed")
     
-    # Convert the JSON response to a string
-    json_string <- toJSON(json_response, pretty = TRUE)
-    json_string
+    # If the API is returning XML:
+    # result <- xml2::as_list(xml2::read_xml(response))
+    
+    # Convert the result to a character vector
+    paste(unlist(result), collapse = ", ")
   })
   
   output$prediction <- renderText({
